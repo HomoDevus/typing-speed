98475950ab70366d2d9a7689ba74007ff89f8a6a
ItisEternity <77201958+ItisEternity@users.noreply.github.com>
2021-06-29 11:14:33 +0300

    Development

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState('');
+    const [wordsAmount, setWordsAmount] = useState(0);
+
+    function handleChange(e) {
+        setText(e.target.value)
+    }
+
+    function calculateWords() {
+        let wordsAmount = 0;
+        let words = text.split(' ')
+        for (let word of words) {
+            if (word) {wordsAmount += 1}
+        }
+        setWordsAmount(wordsAmount)
+    }
+
+    return (
+        <div className="game-area">
+          <h1>Typing Speed Game</h1>
+          <textarea className="typing-area" value={text} onChange={handleChange}/>
+          <h4 className="timer">Time remaining: 00:00</h4>
+          <button className="start-button" onClick={calculateWords}>Start</button>
+          <h2>Word count: {wordsAmount}</h2>
+        </div>
+    )
 }
 
 export default App;
